c17c7219a2c3309b34b61c559b2d21709a0c0769
aicha652 <aichaazlf652@gmail.com>
2024-05-21 12:58:02 +0200

    Make Some Change To Fix Bugs

--- Stock/models.py
+++ Stock/models.py
@@ -15,7 +15,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(500), unique=False, nullable=False)
-    is_admin = db.Column(db.String(120), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
     phone = db.Column(db.String(120), unique=False, nullable=False)
 
     def __repr__(self):
